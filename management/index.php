b784433d9e72cec197efc60a03b55b9148e128d0
datagutten <datagutten@datagutten.net>
2015-06-14 21:15:25 +0200

    Added index page for management tools

---
+++ management/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Management</title>
+</head>
+
+<body>
+<h1>Tools for editing information in the database</h1>
+<?Php
+require '../class.php';
+$comicmanager=new comicmanager;
+$tools=array('edit_categories.php'=>"Add or edit categories",'managecomics_front.php'=>'Add id or category to strips','edit_strip.php'=>'Edit strip info');
+if(isset($_GET['tool']))
+	echo "<h2>{$tools[$_GET['tool']]}</h2>\n";
+
+$comicinfo=$comicmanager->comicinfo_get();
+if($comicinfo!==false)
+{
+	$comic=$comicinfo['id'];
+
+	if(!isset($_GET['tool']))
+	{
+		foreach($tools as $file=>$name)
+		{
+			echo "<a href=\"?comic=$comic&amp;tool=$file\">$name</a><br />\n";
+		}
+	}
+	elseif(isset($tools[$_GET['tool']]))
+	{
+		header('Location: '.$_GET['tool'].'?comic='.$_GET['comic']);
+	}
+	else
+		echo "Invalid tool: {$_GET['tool']}";
+	
+}
+?>
+</body>
+</html>
