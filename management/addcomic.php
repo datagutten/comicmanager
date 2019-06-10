e442c375ae2daf412d5272e6f82806dd54f08e56
datagutten <datagutten@datagutten.net>
2019-06-10 09:09:41 +0200

    Add possible_key_fields to comic_info table

--- management/addcomic.php
+++ management/addcomic.php
@@ -9,8 +9,29 @@ if(isset($_POST['submit']))
 	if(!isset($_POST['keyfield']))
 		$keyfield='id';
 	else
-		$keyfield=preg_replace('/[^a-z0-9]+/','',strtolower($_POST['keyfield'])); //Make a clean keyfield
-	if(isset($_POST['has_categories']) && $_POST['has_categories']==1)
+		$keyfield=preg_replace('/[^a-z0-9_]+/','',strtolower($_POST['keyfield'])); //Make a clean keyfield
+
+	$field_definitions=array('id'=>'INT(5)','customid'=>'INT(5)','original_date'=>'INT(11)');
+	$fields=$_POST['fields'];
+	if(array_search($keyfield,$fields)===false)
+		$fields[]=$keyfield;
+	print_r($field_definitions);
+	print_r(array_keys($field_definitions));
+	foreach($fields as $field)
+	{
+		$key=array_search($field,array_keys($field_definitions));
+		if($key===false)
+		{
+			echo "Invalid field $field\n";
+			continue;
+		}
+
+		$q=sprintf("ALTER TABLE %s ADD COLUMN %s %s DEFAULT NULL",$comic,$field,$field_definitions[$field]);
+		$comicmanager->db->query($q);
+		echo $q."\n";
+	}
+
+	if(isset($_POST['has_categories']))
 	{
 		$has_categories=1;
 		$q_categories='CREATE TABLE `'.$comic.'_categories` (
@@ -26,9 +47,9 @@ if(isset($_POST['submit']))
 	else
 		$has_categories=0;
 	
-	$st_comic_info=$comicmanager->db->prepare("INSERT INTO comic_info (id,name,keyfield,has_categories) VALUES (?,?,?,?)");
-	
-	if(!$st_comic_info->execute(array($comic,$_POST['name'],$keyfield,$has_categories)))
+	$st_comic_info=$comicmanager->db->prepare("INSERT IGNORE INTO comic_info (id,name,keyfield,has_categories,possible_key_fields) VALUES (?,?,?,?,?)");
+	$possible_key_fields=implode(',',$fields);
+	if(!$st_comic_info->execute(array($comic,$_POST['name'],$keyfield,$has_categories,$possible_key_fields)))
 	{
 		$errorinfo=$st_comic_info->errorInfo();
 		trigger_error("Error inserting comic info: {$errorinfo[2]}",E_USER_WARNING);
