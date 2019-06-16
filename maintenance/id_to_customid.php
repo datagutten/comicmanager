2959a097c8194396a3005c36af7381958dee1eaa
datagutten <datagutten@datagutten.net>
2019-06-16 21:10:32 +0200

    Rewrite id to custom id

--- maintenance/id_to_customid.php
+++ maintenance/id_to_customid.php
@@ -6,40 +6,48 @@ if($comicinfo['keyfield']=='id')
 	echo "This tool is only useful for comics using an alternate key field\n";
 else
 {
+	$st_custom_id = $comicmanager->db->prepare(sprintf('SELECT DISTINCT id FROM %s WHERE customid=?', $comicinfo['id']));
 	$comic=$comicinfo['id'];
 	$keyfield=$comicinfo['keyfield'];
-	
-	$st=$comicmanager->db->query("SELECT * FROM $comic WHERE id!=0 AND (customid IS NULL OR id!=customid) GROUP by id ORDER BY id");
-	if($st===false)
+	$st=$comicmanager->query("SELECT * FROM $comic WHERE id!=0 AND (customid IS NULL OR id!=customid) ORDER BY id");
+
+	while($row=$st->fetch())
 	{
-		$errorinfo=$comicmanager->db->errorInfo();
-		trigger_error("SQL error: ".$errorinfo[2],E_USER_ERROR);
-	}
-	$st_id=$comicmanager->db->query("SELECT distinct $keyfield FROM $comic ORDER BY id"); //Get all used customids
-	$customidlist=$st_id->fetchAll(PDO::FETCH_COLUMN);
-
-	$st_strips=$comicmanager->db->query($q="SELECT id,$keyfield FROM $comic WHERE id IS NOT NULL AND $keyfield IS NOT NULL GROUP BY $keyfield"); //Get id and customid relationships
-	$id_and_customid=$st_strips->fetchAll(PDO::FETCH_KEY_PAIR); //key=id, value=customid
-
-	$rows=$st->fetchAll(PDO::FETCH_ASSOC);
-
-	foreach($rows as $row)
-	{
-		if(array_search($row['id'],$customidlist)===false || //Check if the customid for the current id is free
-		(empty($row[$keyfield]) && isset($id_and_customid[$row['id']]))) //Check if other strips the same id has a customid
+		echo '<pre>'.print_r($row, true).'</pre>';
+		//$st_custom_id = $comicmanager->get(array('customid'=>$row['id']), true);
+		$st_custom_id->execute(array($row['id'])); //Find releases with customid similar to this release id
+		$count = $st_custom_id->rowCount();
+		if($count>1)
 		{
-			if($row[$keyfield]!='')
-				$query="UPDATE $comic SET $keyfield={$row['id']} WHERE customid={$row[$keyfield]};";
+			printf("Multiple ids for customid %d:<br />\n", $row['customid']);
+			while($row_id = $st_custom_id->fetch())
+			{
+				echo $row_id[0]."<br />\n";
+			}
+			continue;
+		}
+		elseif($count==1)
+		{
+			$release = $st_custom_id->fetch();
+			if($release['id']!=$row['id']) //Check if the matching release has the same id
+			{
+				printf("Release with customid %d has different id: %d<br />\n", $row['id'], $release['id']);
+				continue;
+			}
 			else
-				$query="UPDATE $comic SET $keyfield={$row['id']} WHERE id={$row['id']};";
-			//$db->query($query) or die(print_r($db->errorInfo(),true));
-			if(isset($_SERVER['HTTP_USER_AGENT']))
-				echo "$query<br />\n";
-			else
-				echo $query."\n";
-			if(isset($_GET['updatedb']))
-				$comicmanager->db->query($query);
+				printf("Release with customid %d and uid %d has same id as customid %d<br />\n", $row['customid'], $row['uid'], $row['id']);
+		}
+		else
+		{
+			printf("Customid %d is free<br />\n", $row['id']);
+		}
+
+		try {
+			$comicmanager->add_or_update(array('customid'=>$row['id'], 'uid'=>$row['uid']));
+		}
+		catch (Exception $e)
+		{
+			echo $e."<br />\n";
 		}
 	}
-	echo "<p><a href=\"?comic=$comic&amp;tool=id_to_customid.php&amp;updatedb\">Update database</a></p>\n";
 }
