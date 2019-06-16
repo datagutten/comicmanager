acfca41a21efbbe7e2cc467828b63c37ea5a01b8
datagutten <datagutten@datagutten.net>
2019-06-16 19:40:56 +0200

    Show errors in image

--- image.php
+++ image.php
@@ -11,15 +11,33 @@ if(!empty($_GET['uid']) || !empty($_GET['release']))
         $_GET['file'] = $comic_manager->imagefile($release);
     }
     catch (Exception $e) {
-        echo $e->getMessage();
+        image_error($e->getMessage());
+        error_log($e);
     }
-
 }
 if(isset($_GET['file'])) {
     $size = getimagesize($_GET['file']);
+    set_error_handler('handler');
     header("Content-type: {$size['mime']}");
 
     $fp = fopen($_GET['file'], 'r');
-    fpassthru($fp);
-    fclose($fp);
+    if($fp!==false)
+    {
+        fpassthru($fp);
+        fclose($fp);
+    }
+}
+
+function image_error($string)
+{
+    $im=imagecreatetruecolor(1000,20);
+    imagefill($im, 0,0, imagecolorallocate($im, 255,255,255));
+    imagestring($im, 4, 0,0, $string, imagecolorallocate($im, 255,0,0));
+    header("Content-type: image/png");
+    imagepng($im);
+}
+
+function handler(/** @noinspection PhpUnusedParameterInspection */ $errno, $errstr)
+{
+    image_error($errstr);
 }
