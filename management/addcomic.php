080f34f457572943e31f5ce37db5d664a2f89bf4
datagutten <datagutten@datagutten.net>
2019-12-22 10:33:58 +0100

    Rewrite addcomic.php with new classes

--- management/addcomic.php
+++ management/addcomic.php
@@ -1,67 +1,28 @@
 <?Php
+
+use datagutten\comicmanager\metadata;
+use datagutten\comicmanager\setup;
+
 require 'class_management.php';
+require '../vendor/autoload.php';
 $comicmanager=new comicmanager;
+$setup = new setup();
 
 if(isset($_POST['submit']))
 {
-	$comic=preg_replace('/[^a-z0-9]+/','',strtolower($_POST['comic'])); //Make a clean comic id
-	$q_comic="CREATE TABLE `$comic` (
-	  `date` int(11) DEFAULT NULL,
-	  `site` varchar(45) NOT NULL,
-	  `uid` int(11) NOT NULL AUTO_INCREMENT, PRIMARY KEY (`uid`))";
-	$comicmanager->query($q_comic);
-	
-	if(!isset($_POST['keyfield']))
-		$keyfield='id';
-	else
-		$keyfield=preg_replace('/[^a-z0-9_]+/','',strtolower($_POST['keyfield'])); //Make a clean keyfield
+    if(empty($_POST['key_field']))
+        die($comicmanager->render('error.twig', array('error'=>'Missing key field')));
+    if(!isset($_POST['extra_keys']))
+        $_POST['extra_keys'] = [];
 
-	$field_definitions=array('id'=>'INT(5)','customid'=>'INT(5)','original_date'=>'INT(11)');
-	$fields=$_POST['fields'];
-	if(array_search($keyfield,$fields)===false)
-		$fields[]=$keyfield;
-	print_r($field_definitions);
-	print_r(array_keys($field_definitions));
-	foreach($fields as $field)
-	{
-		$key=array_search($field,array_keys($field_definitions));
-		if($key===false)
-		{
-			echo "Invalid field $field\n";
-			continue;
-		}
-
-		$q=sprintf("ALTER TABLE %s ADD COLUMN %s %s DEFAULT NULL",$comic,$field,$field_definitions[$field]);
-		$comicmanager->db->query($q);
-		echo $q."\n";
-	}
-
-	if(isset($_POST['has_categories']))
-	{
-		$has_categories=1;
-		$q_categories='CREATE TABLE `'.$comic.'_categories` (
-  `id` int(2) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `visible` int(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;';
-		$comicmanager->query($q_categories,null);
-
-		$comicmanager->query("ALTER TABLE $comic ADD COLUMN category INT(2) NULL DEFAULT NULL",null);
-	}
-	else
-		$has_categories=0;
-	
-	$st_comic_info=$comicmanager->db->prepare("INSERT IGNORE INTO comic_info (id,name,keyfield,has_categories,possible_key_fields) VALUES (?,?,?,?,?)");
-	$possible_key_fields=implode(',',$fields);
 	try {
-        $st_comic_info->execute(array($comic,$_POST['name'],$keyfield,$has_categories,$possible_key_fields));
-        $comicmanager->comicinfo($comic);
+        $setup->createComic(strtolower($_POST['comic']), $_POST['name'], $_POST['key_field'], isset($_POST['has_categories']), $_POST['extra_keys']);
+        header('Location: managecomics_front.php?comic='.$_POST['comic']);
     }
-	catch (PDOException $e)
+	catch (PDOException|InvalidArgumentException $e)
 	{
-		echo $comicmanager->render('error.twig', array('error'=>'Error inserting comic info: '.$e->getMessage()));
+		echo $comicmanager->render('error.twig', array('error'=>'Error adding comic: '.$e->getMessage(), 'trace'=>$e->getTraceAsString()));
 	}
 }
-
-echo $comicmanager->render('add_comic.twig', array('title'=>'Add comic'));
+else
+    echo $comicmanager->render('add_comic.twig', array('title'=>'Add comic', 'key_fields'=> metadata::$valid_key_fields));
