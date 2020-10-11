819a9f3876c884a06c107149857873bfe7094126
datagutten <datagutten@datagutten.net>
2020-10-11 20:39:33 +0200

    Add test config for SQLite and MySQL

--- tests/test_config_db.php
+++
@@ -1,7 +0,0 @@
-<?Php
-$config['db_host']="localhost";
-$config['db_user']="php_test";
-$config['db_password']="password";
-$config['db_name']='comicmanager_test';
-$config['db_type']='mysql';
-return $config;
