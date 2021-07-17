2d2e455e45afb222c6ab1481f11806e27928bee9
datagutten <datagutten@datagutten.net>
2021-07-17 13:13:21 +0200

    Add GitHub actions workflow

--- tests/test_config_mysql.php
+++ tests/test_config_mysql.php
@@ -5,10 +5,18 @@ $config['comics']['site_url'] = getenv('comics_site');
 $config['comics']['secret_key'] = getenv('comics_key');
 $config['debug'] = true;
 
+if (!empty(getenv('DB_DATABASE')))
+{
+    $config['db']['db_user'] = getenv('DB_USER');
+    $config['db']['db_password'] = getenv('DB_PASSWORD');
+    $config['db']['db_name'] = getenv('DB_DATABASE');
+} else
+{
+    $config['db']['db_user'] = "php_test";
+    $config['db']['db_password'] = "password";
+    $config['db']['db_name'] = 'comicmanager_test';
+}
 $config['db']['db_host'] = "localhost";
-$config['db']['db_user'] = "php_test";
-$config['db']['db_password'] = "password";
-$config['db']['db_name'] = 'comicmanager_test';
 $config['db']['db_type'] = 'mysql';
 
 $config['comics']['db'] = $config['db'];
