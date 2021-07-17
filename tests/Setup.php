5a362f36701feb7a472b749971cf9a6cf0f6f306
datagutten <datagutten@datagutten.net>
2021-07-17 21:21:34 +0200

    Create comics cache table

--- tests/Setup.php
+++ tests/Setup.php
@@ -4,17 +4,19 @@
 namespace datagutten\comicmanager\tests;
 
 
+use datagutten\comics_tools\comics_api_client\ComicsAPICache;
+
 class Setup extends common
 {
     function setUp(): void
     {
         parent::setUp();
-        $this->config['comics'] = null;
         if (!file_exists($this->config['file_path']))
             mkdir($this->config['file_path']);
 
         $setup = new \datagutten\comicmanager\setup(['id' => 'pondus', 'name' => 'Pondus', 'key_field' => 'customid', 'has_categories' => true, 'possible_key_fields' => ['id', 'customid']], $this->config);
         $setup->createComicInfoTable();
         $setup->create();
+        ComicsAPICache::create_table($setup->db);
     }
 }
