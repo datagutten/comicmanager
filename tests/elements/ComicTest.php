80c82a59980ede00e83c566569ce0bc4aa18ea89
datagutten <datagutten@datagutten.net>
2021-07-18 10:08:35 +0200

    Add more tests for Comic to get 100% coverage

--- tests/elements/ComicTest.php
+++ tests/elements/ComicTest.php
@@ -4,14 +4,64 @@ namespace datagutten\comicmanager\tests\elements;
 
 use datagutten\comicmanager\elements\Comic;
 use datagutten\comicmanager\tests\Setup;
+use InvalidArgumentException;
 
 class ComicTest extends Setup
 {
+    /**
+     * @var Comic
+     */
+    public Comic $comic;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->comic = Comic::from_db($this->db, 'pondus');
+    }
+
     public function testFrom_db()
     {
-        $comic = Comic::from_db($this->db, 'pondus');
-        $this->assertEquals('pondus', $comic->id);
-        $this->assertFalse(empty($comic->id));
-        $this->assertFalse(empty($comic['id']));
+        $this->assertEquals('pondus', $this->comic->id);
+        $this->assertFalse(empty($this->comic->id));
+        $this->assertFalse(empty($this->comic['id']));
+    }
+
+    public function testAllowedKeyField()
+    {
+        $allowed = $this->comic->allowedKeyField('customid');
+        $this->assertSame('Custom grouping id', $allowed);
+    }
+
+    public function testNotAllowedKeyField()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('original_date is not a valid key field for pondus');
+        $this->comic->allowedKeyField('original_date');
+    }
+
+    public function testSetKeyField()
+    {
+        $this->assertSame('customid', $this->comic->key_field);
+        $this->comic->setKeyField('id');
+        $this->assertSame('id', $this->comic->key_field);
+    }
+
+    public function testArraySetInvalidKeyField()
+    {
+        $this->expectExceptionMessage('Invalid key field: foo');
+        $this->comic['key_field'] = 'foo';
+    }
+
+    public function testArraySetInvalidId()
+    {
+        $this->expectExceptionMessage('Invalid comic id: foo+');
+        $this->comic['id'] = 'foo+';
+    }
+
+    public function testUnset()
+    {
+        $this->assertSame('customid', $this->comic->key_field);
+        unset($this->comic['key_field']);
+        $this->assertFalse(isset($this->comic->key_field));
     }
 }
